c080b94c835d7f34c9cf415ba841bc86d4f17eab
ayush-1412 <prof.ayush14@gmail.com>
2026-01-18 22:28:33 +0530

    Fix mixed content by using HTTPS backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
 
   const handleSuccess = async (credentialResponse) => {
     try {
-      const res = await axios.post(
+      const res = await api.post(
         "https://wishers-backend.onrender.com/api/auth/google",
         {
           token: credentialResponse.credential,
