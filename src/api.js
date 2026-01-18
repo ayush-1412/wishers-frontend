320e38d3c79422b2a589da86e392bca675565a25
ayush-1412 <prof.ayush14@gmail.com>
2026-01-18 22:36:07 +0530

    Fix mixed content by using HTTPS backend

--- src/api.js
+++ src/api.js
@@ -4,15 +4,12 @@ const api = axios.create({
   baseURL: "https://wishers-backend.onrender.com",
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
 
 export default api;
